f322fc3c12006c2bcac4d186aa75ca7b857b507d
K e n V <kenneth_valdivia@yahoo.com>
2019-12-29 23:37:40 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -4,3 +4,7 @@ def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+grand_total = 0
+ row_index = 0
+ while row_index < vm.length do
